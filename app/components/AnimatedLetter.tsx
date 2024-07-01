ba81cf0e9e042cccd4b87fd791eea365b8257498
hetarho <sunlikeperson@gmail.com>
2024-07-01 20:41:07 +0900

    [ADD] image

---
+++ app/components/AnimatedLetter.tsx
@@ -0,0 +1,61 @@
+import { animate } from "framer-motion";
+import { useCallback, useEffect, useState } from "react";
+
+export default function AnimatedLetter({
+  letter,
+  delay,
+  duration,
+  pause,
+}: {
+  letter: string;
+  delay: number;
+  duration: number;
+  pause: number;
+}) {
+  const [fontWeight, setFontWeight] = useState(100);
+
+  const _forwardAnim = useCallback(() => {
+    animate(100, 900, {
+      duration: duration,
+      onUpdate(latest) {
+        setFontWeight(latest);
+      },
+      onComplete() {
+        _backwardAnim();
+      },
+    });
+  }, []);
+
+  const _backwardAnim = useCallback(() => {
+    animate(900, 100, {
+      duration: duration,
+      onUpdate(latest) {
+        setFontWeight(latest);
+      },
+      onComplete() {
+        setTimeout(() => {
+          _forwardAnim();
+        }, pause * 1000);
+      },
+    });
+  }, []);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      _forwardAnim();
+    }, delay);
+
+    return () => clearTimeout(timer);
+  }, [delay, _forwardAnim]);
+
+  return (
+    <span
+      className="block"
+      style={{
+        fontVariationSettings: `'wght' ${fontWeight}, 'opsz' ${fontWeight}`,
+      }}
+    >
+      {letter}
+    </span>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -14,24 +14,26 @@ import {
   RefObject,
   useCallback,
   useEffect,
+  useRef,
   useState,
 } from "react";
 import { AnimationItem } from "./types/custom/lottie.type";
 import clsx from "clsx";
 import Image from "next/image";
 import Modal from "./components/modal";
-import { animate, motion } from "framer-motion";
+import { animate, motion, useAnimate, useInView } from "framer-motion";
+import AnimatedLetter from "./components/AnimatedLetter";
 
 export default function Home() {
-  const letters = "저희의 결혼식에 여러분을 초대합니다.".split("");
+  const letters = "저희의 결혼식에 여러분을 초대합니다".split("");
 
   return (
     <main className="">
-      <div className="mt-10 flex justify-center text-4xl font-thin">
+      <div className="mt-16 flex justify-center text-6xl font-thin">
         <span className="font-light">12</span>월
         <span className="font-light">14</span>일
       </div>
-      <div className="mt-1 flex justify-center items-end text-2xl">
+      <div className="mt-3 flex justify-center items-end text-2xl font-light">
         <span>이해람</span>
         <Lottie
           className="h-12"
@@ -41,101 +43,77 @@ export default function Home() {
         />
         <span>임예은</span>
       </div>
-      <div className="flex flex-col items-center mt-8 text-3xl">
+      <div className="flex flex-col items-center mt-12 text-5xl">
         {letters.map((letter, index) =>
           letter == " " ? (
-            <div key={index} className="h-2"></div>
+            <div key={index} className="h-4"></div>
           ) : (
             <AnimatedLetter
               key={index}
               letter={letter}
-              delay={index * 110}
-              duration={1.2}
-              pause={3}
+              delay={index * 100}
+              duration={1.1}
+              pause={2.5}
             />
           )
         )}
       </div>
+      {Array(9)
+        .fill((_: any) => 1)
+        .map((_, i) => (
+          <MyImage key={i} index={i}></MyImage>
+        ))}
     </main>
   );
 }
 
-function MyImage({
-  url,
-  onClick,
-  layoutId,
-}: {
-  url: string;
-  layoutId: string;
-  onClick: MouseEventHandler<HTMLImageElement>;
-}) {
+function MyImage({ index }: { index: number }) {
+  const [scope, animate] = useAnimate();
+  const isInView = useInView(scope);
+
+  useEffect(() => {
+    if (isInView) {
+      animate(scope.current, { opacity: 1 }, { duration: 2 });
+      animate(scope.current, { translate: "0" }, { duration: 1 });
+      animate(scope.current, { rotate: "180deg" }, { duration: 0.5 });
+    }
+  }, [animate, isInView, scope]);
+
   return (
-    <motion.div
-      key={url}
-      className="aspect-square w-full overflow-hidden rounded-2xl"
-      layoutId={layoutId}
-      onClick={onClick}
-    >
-      <Image src={url} alt="" width={500} height={500}></Image>
-    </motion.div>
+    <div className="flex items-center justify-center w-full h-60">
+      <div
+        ref={scope}
+        className="w-48 h-48 bg-pink-400 opacity-0 rounded-3xl overflow-hidden"
+        style={{ rotate: "180deg", translate: "0 180px" }}
+      >
+        <Image
+          alt={`${index}`}
+          src={`/imgs/${index}.jpeg`}
+          width={500}
+          height={500}
+        ></Image>
+      </div>
+    </div>
   );
 }
 
-const AnimatedLetter = ({
-  letter,
-  delay,
-  duration,
-  pause,
-}: {
-  letter: string;
-  delay: number;
-  duration: number;
-  pause: number;
-}) => {
-  const [fontWeight, setFontWeight] = useState(100);
-
-  const _forwardAnim = useCallback(() => {
-    animate(100, 900, {
-      duration: duration,
-      onUpdate(latest) {
-        setFontWeight(latest);
-      },
-      onComplete() {
-        _backwardAnim();
-      },
-    });
-  }, []);
-
-  const _backwardAnim = useCallback(() => {
-    animate(900, 100, {
-      duration: duration,
-      onUpdate(latest) {
-        setFontWeight(latest);
-      },
-      onComplete() {
-        setTimeout(() => {
-          _forwardAnim();
-        }, pause * 1000);
-      },
-    });
-  }, []);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      _forwardAnim();
-    }, delay);
-
-    return () => clearTimeout(timer);
-  }, [delay, _forwardAnim]);
-
-  return (
-    <span
-      className="block"
-      style={{
-        fontVariationSettings: `'wght' ${fontWeight}, 'opsz' ${fontWeight}`,
-      }}
-    >
-      {letter}
-    </span>
-  );
-};
+// function MyImage({
+//   url,
+//   onClick,
+//   layoutId,
+// }: {
+//   url: string;
+//   layoutId: string;
+//   onClick: MouseEventHandler<HTMLImageElement>;
+// }) {
+//   return (
+//     <motion.div
+//       key={url}
+//       className="aspect-square w-full overflow-hidden rounded-2xl"
+//       layoutId={layoutId}
+//       onClick={onClick}
+//     >
+//       <Image src={url} alt="" width={500} height={500}></Image>
+//     </motion.div>
+//   );
+// }
