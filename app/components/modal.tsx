ce70c7e1f01414bc7e15e93e816f6ba2a6c17cf6
hetarho <sunlikeperson@gmail.com>
2024-07-01 15:43:44 +0900

    [ADD] remove all

--- app/components/modal.tsx
+++ app/components/modal.tsx
@@ -1,5 +1,4 @@
 import { motion } from "framer-motion";
-import Image from "next/image";
 import React, { useEffect, useState } from "react";
 
 interface ModalProps {
@@ -20,16 +19,6 @@ const Modal: React.FC<ModalProps> = ({ imageIdx, isOpen, onClose }) => {
     setUrl(`/imgs/${selectedIdx}.jpeg`);
   }, [selectedIdx]);
 
-  function onClickLeftArrow() {
-    if (selectedIdx > 0) {
-      setSelectedIdx((prev) => prev - 1);
-    }
-  }
-  function onClickRightArrow() {
-    if (selectedIdx < 8) {
-      setSelectedIdx((prev) => prev + 1);
-    }
-  }
   if (!isOpen) return null;
 
   return (
@@ -47,54 +36,8 @@ const Modal: React.FC<ModalProps> = ({ imageIdx, isOpen, onClose }) => {
       >
         <motion.img src={url} />
       </motion.div>
-      <div
-        className="absolute left-2"
-        onClick={(e) => {
-          e.stopPropagation();
-          onClickLeftArrow();
-        }}
-      >
-        <FormkitLeft />
-      </div>
-      <div
-        className="absolute right-2"
-        onClick={(e) => {
-          e.stopPropagation();
-          onClickRightArrow();
-        }}
-      >
-        <FormkitRight />
-      </div>
     </motion.div>
   );
 };
 
 export default Modal;
-
-const FormkitRight = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="4em"
-    height="4em"
-    viewBox="0 0 7 16"
-  >
-    <path
-      fill="#F0F0F0"
-      d="M1.5 13a.47.47 0 0 1-.35-.15c-.2-.2-.2-.51 0-.71L5.3 7.99L1.15 3.85c-.2-.2-.2-.51 0-.71c.2-.2.51-.2.71 0l4.49 4.51c.2.2.2.51 0 .71l-4.5 4.49c-.1.1-.23.15-.35.15Z"
-    ></path>
-  </svg>
-);
-
-const FormkitLeft = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="4em"
-    height="4em"
-    viewBox="0 0 7 16"
-  >
-    <path
-      fill="#F0F0F0"
-      d="M5.5 13a.47.47 0 0 1-.35-.15l-4.5-4.5c-.2-.2-.2-.51 0-.71l4.5-4.49c.2-.2.51-.2.71 0c.2.2.2.51 0 .71L1.71 8l4.15 4.15c.2.2.2.51 0 .71c-.1.1-.23.15-.35.15Z"
-    ></path>
-  </svg>
-);
